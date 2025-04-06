5c2bce1e1125d48d7725133d253e9f588e45966c
Kirill Bychkov <kkbychkov@gmail.com>
2025-04-06 17:11:07 +0300

    Added main

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "Automata.h"
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string menu[]{ "Espresso", "Americano", "Coconut milk raf" };
+    int prices[]{ 80, 20, 450 };
+    Automata DrinkMachine(menu, prices, 3);
+    std::cout << DrinkMachine.On();
+    std::cout << DrinkMachine.etMenu();
+    std::cout << DrinkMachine.Coin(50);
+    std::cout << DrinkMachine.Choice(1);
+    std::cout << DrinkMachine.Choice(0);
+    std::cout << DrinkMachine.Coin(400);
+    std::cout << DrinkMachine.Choice(2);
+    std::cout << DrinkMachine.Check();
+    std::cout << DrinkMachine.Coin(400);
+    std::cout << DrinkMachine.Choice(2);
+    std::cout << DrinkMachine.Check();
+    std::cout << DrinkMachine.Cook();
+    std::cout << DrinkMachine.Finish();
+}
