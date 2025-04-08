87c61bf2370e0ca85eef99478f4e9ecdcbac5867
Kirill Bychkov <kkbychkov@gmail.com>
2025-04-08 12:21:59 +0300

    Release try 3

--- include/Automata.h
+++ include/Automata.h
@@ -6,31 +6,33 @@
 
 class Automata {
     private:
-         enum STATES {
-             OFF,
-             WAIT,
-             ACCEPT,
-             CHECK,
-             COOK,
-             COLLECT
-         };
-         int selected;
-         int balance;
-         std::string* menu;
-         int* price;
-         STATES state;
-         int sizeMenu;
+        enum STATES {
+            OFF,
+            WAIT,
+            ACCEPT,
+            CHECK,
+            COOK,
+            COLLECT
+        };
+
+        int selected;
+        int balance;
+        std::string* menu;
+        int* price;
+        STATES state;
+        int sizeMenu;
 
     public:
-         Automata(std::string menu[], int prices[], int size);
-         std::string On();
-         std::string Off();
-         std::string Coin(int);
-         std::string GetMenu();
-         std::string getState();
-         std::string Choice(int);
-         std::string Check();
-         std::string Cancel();
-         std::string Cook();
-         std::string Finish();
+        Automata(std::string menu[], int prices[], int size);
+        
+        std::string On();
+        std::string Off();
+        std::string Coin(int);
+        std::string GetMenu();
+        std::string getState();
+        std::string Choice(int);
+        std::string Check();
+        std::string Cancel();
+        std::string Cook();
+        std::string Finish();
 };
